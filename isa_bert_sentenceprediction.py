798f27a88e945e504b7897634f22c4ff5c355cc7
isabeauoliveri <isabeauoliveri11@yahoo.it>
2020-04-01 13:03:02 +0200

    prova commit

--- isa_bert_sentenceprediction.py
+++ isa_bert_sentenceprediction.py
@@ -1,9 +1,13 @@
+# DON'T RUN 
+
 # Sequence Classification with BERT
 # Huggingface library
 # https://huggingface.co/transformers/
 # Also taking ispiration from
 # https://towardsdatascience.com/bert-classifier-just-another-pytorch-model-881b3cf05784
 # https://github.com/sugi-chan/custom_bert_pipeline/blob/master/bert_pipeline.ipynb
+
+# Model for BertForSequenceClassification
 
 class BertForSequenceClassification(nn.Module):
   
@@ -20,4 +24,106 @@ class BertForSequenceClassification(nn.Module):
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
 
+      
         return logits
+
+# Train model function - Return the trained model
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+    print('starting')
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = 100
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            
+            output_corrects = 0
+            
+            
+            # Iterate over data.
+            for inputs, output in dataloaders_dict[phase]:
+                #inputs = inputs
+                #print(len(inputs),type(inputs),inputs)
+                #inputs = torch.from_numpy(np.array(inputs)).to(device) 
+                inputs = inputs.to(device) 
+
+                output = output.to(device)
+                
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    #print(inputs)
+                    outputs = model(inputs)
+
+                    outputs = F.softmax(outputs,dim=1)
+                    
+                    loss = criterion(outputs, torch.max(output.float(), 1)[1])
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+
+                
+                output_corrects += torch.sum(torch.max(outputs, 1)[1] == torch.max(output, 1)[1])
+
+                
+            epoch_loss = running_loss / dataset_sizes[phase]
+
+            
+            output_acc = output_corrects.double() / dataset_sizes[phase]
+
+            print('{} total loss: {:.4f} '.format(phase,epoch_loss ))
+            print('{} output_acc: {:.4f}'.format(
+                phase, sentiment_acc))
+
+            if phase == 'val' and epoch_loss < best_loss:
+                print('saving with loss of {}'.format(epoch_loss),
+                      'improved over previous {}'.format(best_loss))
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+                torch.save(model.state_dict(), 'bert_model_test.pth')
+
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(float(best_loss)))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+# Reading the dataset
+
+import pandas as pd
+
+dat = pd.read_csv('dataset_example_chunk_recsys2020.csv')
+dat.head()
+
+# Tokenization phase is not necessary
+# because we have already the token IDS
+# -> tokenizer.tokenize  NOT NECESSARY
+# -> tokenizer.convert_tokens_to_ids NOT NECESSARY
+
