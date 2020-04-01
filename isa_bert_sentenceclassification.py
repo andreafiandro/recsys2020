3483bf35df9eb2af24fc4da8579b69bd0bb07fb1
isabeauoliveri <isabeauoliveri11@yahoo.it>
2020-04-01 18:19:43 +0200

    bert sequence classification

--- isa_bert_sentenceclassification.py
+++ isa_bert_sentenceclassification.py
@@ -11,20 +11,9 @@
 
 
 import torch
-from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
-
-# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
-import logging
-logging.basicConfig(level=logging.INFO)
-
-# Load pre-trained model tokenizer (vocabulary)
-# Tokenization phase is not necessary
-# because we have already the token IDS
-# -> tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased') NOT NECESSARY
-
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM, BertConfig
+import logging # Optional: if you want to have more information on what's happening, activate the logger as follows
 from __future__ import print_function, division
-
-# COMM import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
@@ -39,6 +28,15 @@ from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 from random import randrange
 import torch.nn.functional as F
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO)
+# Load pre-trained model tokenizer (vocabulary)
+# Tokenization phase is not necessary
+# because we have already the token IDS
+# -> tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased') NOT NECESSARY
 
 # Model for Bert Layer Normalization  
 
@@ -171,7 +169,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     return model
 
 
-# CHECK IF MAX SEQ LENGTH IS CORRECT 
+# Please, check if max sequence length is correct
+
 max_seq_length = 512
 
 # Bring the dataset to a useful format
@@ -186,12 +185,20 @@ class text_dataset(Dataset):
         
         # Tokenization phase is not necessary
         # because we have already the token IDS
+        # -> tokenizer.tokenize  NOT NECESSARY
+        # -> tokenizer.convert_tokens_to_ids NOT NECESSARY
+
         # -> tokenized_review = tokenizer.tokenize(self.x_y_list[0][index]) NOT NECESSARY
-        
+        # so:
+        tokenized_review = self.x_y_list[0][index]
+
         if len(tokenized_review) > max_seq_length:
             tokenized_review = tokenized_review[:max_seq_length]
             
-        ids_review  = tokenizer.convert_tokens_to_ids(tokenized_review)
+        # -> ids_review  = tokenizer.convert_tokens_to_ids(tokenized_review) NOT NECESSARY
+        # so:
+
+        ids_review  = tokenized_review
 
         padding = [0] * (max_seq_length - len(ids_review))
         
@@ -199,11 +206,11 @@ class text_dataset(Dataset):
         
         assert len(ids_review) == max_seq_length
         
-        #print(ids_review)
+        print(ids_review)
         ids_review = torch.tensor(ids_review)
         
-        sentiment = self.x_y_list[1][index] # color        
-        list_of_labels = [torch.from_numpy(np.array(sentiment))]
+        output = self.x_y_list[1][index] # color        
+        list_of_labels = [torch.from_numpy(np.array(output))]
         
         
         return ids_review, list_of_labels[0]
@@ -211,57 +218,75 @@ class text_dataset(Dataset):
     def __len__(self):
         return len(self.x_y_list[0])
 
-# Reading the dataset
 
-import pandas as pd
-from pytorch_pretrained_bert import BertConfig
+#####################################################################################
+"""MAIN START HERE"""
+#####################################################################################
 
-dat = pd.read_csv('training_chunk_2.csv')
-dat.head()
+# Config Bert and BertForSequenceClassification Model
 
 config = BertConfig(vocab_size_or_config_json_file=32000, hidden_size=768,
         num_hidden_layers=12, num_attention_heads=12, intermediate_size=3072)
 
+# Here will change the number of labels and its format to obtain one or four model, and have as return value logits or CE loss + labels
+
 num_labels = 2
 model = BertForSequenceClassification(num_labels)
 
-# Tokenization phase is not necessary
-# because we have already the token IDS
-# -> tokenizer.tokenize  NOT NECESSARY
-# -> tokenizer.convert_tokens_to_ids NOT NECESSARY
+# Reading the dataset
 
-# Convert inputs to PyTorch tensors
+# Open dataset training chunk and preview of content
 
-tokens_tensor = torch.tensor(list_sequences_token_ids)
-logits = model(tokens_tensor)
+dat = pd.read_csv('isa_puppy_chunk_recsys2020.csv')
+dat.head()
 
-# COMM import torch.nn.functional as F
+# Select the columns of our interest, text tokens and tweet type
 
-F.softmax(logits,dim=1)
-
-from sklearn.model_selection import train_test_split
 X = dat['Text tokens']
 y = dat['Tweet type']
 
+# Split in train and test part the chunk
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+# In this case, due the nature of text tokens field, we will have a list of string. 
+# Each string is a sequence of token ids separed by | , that have to be correctly transformed into a list 
+# (this will be done by text_data function).
+
 X_train = X_train.values.tolist()
 X_test = X_test.values.tolist()
+
+# pandas.get_dummies
+# Convert categorical variable into dummy/indicator variables.
+# Examples
+#
+# s = pd.Series(list('abca'))
+# pd.get_dummies(s)
+#    a  b  c
+# 0  1  0  0
+# 1  0  1  0
+# 2  0  0  1
+# 3  1  0  0
+# pd.get_dummies(s).values.tolist()
+# [[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 0, 0]]
+#
+# ... it is like a one hot encoding for labels
 
 y_train = pd.get_dummies(y_train).values.tolist()
 y_test = pd.get_dummies(y_test).values.tolist()
 
-
-
-# Choose a batch size consider the chunck size
+# Choose a batch size considering the chunck size
 
 batch_size = 16
+
+# Input preparation for Dataloader function
 
 train_lists = [X_train, y_train]
 test_lists = [X_test, y_test]
 
-training_dataset = text_dataset(x_y_list = train_lists )
+# Here, we perform the transformation needed over the input
 
+training_dataset = text_dataset(x_y_list = train_lists )
 test_dataset = text_dataset(x_y_list = test_lists )
 
 dataloaders_dict = {'train': torch.utils.data.DataLoader(training_dataset, batch_size=batch_size, shuffle=True, num_workers=0),
@@ -274,4 +299,37 @@ dataset_sizes = {'train':len(train_lists[0]),
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model = train_model(model, criterion, optimizer, scheduler, num_epochs=25)
+lrlast = .001
+lrmain = .00001
+optim1 = optim.Adam(
+    [
+        {"params":model.bert.parameters(),"lr": lrmain},
+        {"params":model.classifier.parameters(), "lr": lrlast},
+       
+   ])
+
+# optim1 = optim.Adam(model.parameters(), lr=0.001)#,momentum=.9)
+# Observe that all parameters are being optimized
+optimizer_ft = optim1
+criterion = nn.CrossEntropyLoss()
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=3, gamma=0.1)
+
+model_ft1 = train_model(model, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=10)
+
+
+####################################################
+"""COSE CHE NON SO DOVE METTERE, UN ATTIMO """
+####################################################
+
+# Convert inputs to PyTorch tensors
+tokens_tensor = torch.tensor(list_sequences_token_ids)
+logits = model(tokens_tensor)
+
+# COMM import torch.nn.functional as F
+
+F.softmax(logits,dim=1)
+
+
