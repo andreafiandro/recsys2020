10ddd238a0f6df241a8945fa96c974af163deb3a
andreafiandro <andrea.fiandro@gmail.com>
2020-05-12 11:00:13 +0200

    Fix explode

--- Gradient_Boosting/RecsysStats.py
+++ Gradient_Boosting/RecsysStats.py
@@ -317,7 +317,7 @@ class RecSysStats:
         dd_input = dd_input[['User_id', 'Hashtags']]
         dd_input = dd_input.dropna(subset=['Hashtags'])
         dd_input['Hashtags'] = dd_input['Hashtags'].apply(lambda x: [] if(x == 0) else x.split('|'))
-        dd_input['Hashtags'] = dd_input['Hashtags'].explode().compute()
+        dd_input = dd_input.explode('Hashtags').compute()
         dd_input = dd_input.drop_duplicates()
         print(dd_input.head())
 
